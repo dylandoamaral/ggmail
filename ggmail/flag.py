8b0befe1748c1a1408ec67081df057b89eaefaf9
dylandoamaral <do.amaral.dylan@gmail.com>
2021-10-10 15:26:39 +0200

    Add remove and add flag for message

--- ggmail/flag.py
+++ ggmail/flag.py
@@ -2,8 +2,8 @@ from enum import Enum
 
 
 class Flag(Enum):
-    ANSWERED = "Answered"
-    DELETED = "Deleted"
-    DRAFT = "Draft"
-    FLAGGED = "Flagged"
-    SEEN = "Seen"
+    ANSWERED = "\\Answered"
+    DELETED = "\\Deleted"
+    DRAFT = "\\Draft"
+    FLAGGED = "\\Flagged"
+    SEEN = "\\Seen"
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,6 @@
+from imaplib import IMAP4_SSL
+from unittest.mock import ANY, Mock, call, patch
+
 from pytest import fixture
 
 from ggmail.account import Account
@@ -6,3 +9,10 @@ from ggmail.account import Account
 @fixture
 def account():
     return Account(username="test@gmail.com", password="secret")
+
+
+@fixture
+@patch.object(IMAP4_SSL, "login")
+def logged_account(imap_login_mock, account):
+    account.login()
+    return account
